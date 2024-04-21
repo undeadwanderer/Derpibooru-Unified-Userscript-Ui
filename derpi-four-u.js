c446e28dbc80c491efb99af82421f83a44cb65b5
undeadwanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-04-22 01:01:41 +0300

    Update derpi-four-u.js

--- derpi-four-u.js
+++ derpi-four-u.js
@@ -329,6 +329,7 @@ var ConfigManager = (function () {
       const btn = e.target;
       const scriptId = btn.dataset.scriptId;
       const reader = new FileReader();
+      reader.readAsText(file);
       reader.onload = function() {
         console.log('Reader loaded');
         const storage = getStorage();
@@ -351,9 +352,6 @@ var ConfigManager = (function () {
         setStorage(storage);
         console.log('input text is:' + reader.result);
       };
-      if (file) {
-        reader.readAsText(file);
-      }
       // importBtn.innerHTML = 'Uploaded!';
     });
 
