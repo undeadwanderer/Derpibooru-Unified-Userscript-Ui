57b79072cbed003a004ed6bc88db93fec2c465ec
undeadwanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-04-21 22:41:08 +0300

    Update derpi-four-u.js

--- derpi-four-u.js
+++ derpi-four-u.js
@@ -309,7 +309,7 @@ var ConfigManager = (function () {
         // copy(JSON.stringify(storage));
         // exportBtn.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(storage));
         // exportBtn.download = '${LIBRARY_ID}.json';
-        exportBtn.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(storage[LIBRARY_ID])));
+        exportBtn.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(storage)));
         exportBtn.setAttribute('download', `${LIBRARY_ID}.json`);
       }
         // exportBtn.innerHTML = 'Downloaded!';
