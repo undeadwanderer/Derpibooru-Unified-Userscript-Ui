3b12ffc7976c8672cd8e5be8c0edab2306536a67
undeadwanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-04-22 16:35:32 +0300

    Update derpi-four-u.js

--- derpi-four-u.js
+++ derpi-four-u.js
@@ -366,11 +366,26 @@ var ConfigManager = (function () {
             for (const input of inputElements) {
               const key = input.dataset.entryKey;
               const propType = input.dataset.entryPropertyType;
-              let inputValue = input[propType].value;
+              const elemType = input.getAttribute('type')
+              let inputValue = input[propType];
 
-              inputValue = storage[scriptId][key];
+              //  input[type="checkbox"] accepts boolean values, but data-default-value stores 'true' 'false' strings.
+              if (elemType == 'checkbox') {
+                inputValue.checked = storage[scriptId][key];
+              } else if (elemType == 'number') { //  input[type="number"] uses valueAsNumber property for reading and storing values.
+                inputValue.innerHTML = Number.parseFloat(storage[scriptId][key]);
+              } else if (elemType == 'dropdown') {
+                inputValue.value = storage[scriptId][key]
+              } else if (elemType == 'radio') {
+                if inputValue.value == storage[scriptId][key] {
+                  inputValue.checked = true;
+                // } else {
+                  // inputValue.checked = false;
+                }
+              } else {
+                inputValue.innerHTML = storage[scriptId][key];
+              }
             }
-          }
           // let tabContents = document.querySelector('div[data-tab=userscript]');
           // console.log('fetched tab = ' + JSON.stringify(tabContents.innerHTML));
           // for (const j of tabContents.children) {
